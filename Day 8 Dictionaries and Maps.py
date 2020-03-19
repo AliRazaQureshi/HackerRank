ef36b8bca1aed05fe054b493f34c94ac93fbdd5f
AliRazaQureshi <47821624+AliRazaQureshi@users.noreply.github.com>
2020-03-19 17:20:44 +0500

    second commit

--- Day 8 Dictionaries and Maps.py
+++ Day 8 Dictionaries and Maps.py
@@ -1,3 +1,4 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
 def create_phonebook(n):
     phoneBook = dict()
     while n > 0:
@@ -5,11 +6,14 @@ def create_phonebook(n):
         phoneBook[user_input[0]] = user_input[1]
         n -= 1
     return phoneBook
-    
+
 n = int(input())
 phoneBook = create_phonebook(n)
 while True:
-    name = input()
+    try:
+        name = input()
+    except EOFError:
+        break
     if name in phoneBook:
         print("{}={}".format(name, phoneBook[name]))
     else:
