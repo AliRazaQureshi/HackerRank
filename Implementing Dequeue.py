31dce8574182b7abb0e81a1d640ac6196142df01
AliRazaQureshi <47821624+AliRazaQureshi@users.noreply.github.com>
2020-03-25 13:45:43 +0500

    Dequeue

---
+++ Implementing Dequeue.py
@@ -0,0 +1,33 @@
+class Dequeue:
+    def __init__(self):
+        self.items = []
+
+    def add_front(self, item):
+        return self.items.append(item)
+
+    def add_rear(self, item):
+        return self.items.insert(0, item)
+
+    def remove_front(self):
+        return self.items.pop()
+
+    def remove_rear(self):
+        return self.items.pop(0)
+
+    def is_empty(self):
+        return self.items == []
+
+    def size(self):
+        return len(self.items)
+
+d = Dequeue()
+print(d.is_empty())
+print(d.add_rear(4))
+print(d.add_rear('dog'))
+print(d.add_front('cat'))
+print(d.add_front(True))
+print(d.size())
+print(d.is_empty())
+print(d.add_rear(8.4))
+print(d.remove_rear())
+print(d.remove_front())
